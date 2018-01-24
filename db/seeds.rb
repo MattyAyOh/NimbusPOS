804f85443417efa8493cc520201a36762d1db514
Calliope <thecalliopecrashed@gmail.com>
2018-01-24 15:30:51 -0500

    List default services and extras in `db/seeds`
    These are taken from the example app on Heroku:
    https://nimbus-pos.herokuapp.com/services

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# First, remove old data
+Service.destroy_all
+Extra.destroy_all
+
+# List out the available services
+Service.create!(name: "Mahjong 1", hourly_rate: 0.0, service_type: "mahjong")
+Service.create!(name: "Mahjong 2", hourly_rate: 0.0, service_type: "mahjong")
+Service.create!(name: "Mahjong 3", hourly_rate: 0.0, service_type: "mahjong")
+Service.create!(name: "Mahjong 4", hourly_rate: 0.0, service_type: "mahjong")
+Service.create!(name: "Mahjong 5", hourly_rate: 0.0, service_type: "mahjong")
+Service.create!(name: "Mahjong 6", hourly_rate: 0.0, service_type: "mahjong")
+Service.create!(name: "Mahjong 7", hourly_rate: 0.0, service_type: "mahjong")
+Service.create!(name: "Mahjong 8", hourly_rate: 0.0, service_type: "mahjong")
+
+Service.create!(name: "Pool 1", hourly_rate: 0.0, service_type: "ktv")
+Service.create!(name: "Pool 2", hourly_rate: 0.0, service_type: "ktv")
+Service.create!(name: "Pool 3", hourly_rate: 0.0, service_type: "ktv")
+Service.create!(name: "Pool 4", hourly_rate: 0.0, service_type: "ktv")
+
+Service.create!(name: "KTV 1", hourly_rate: 0.0, service_type: "pool")
+Service.create!(name: "KTV 2", hourly_rate: 0.0, service_type: "pool")
+Service.create!(name: "KTV 3", hourly_rate: 0.0, service_type: "pool")
+Service.create!(name: "KTV 4", hourly_rate: 0.0, service_type: "pool")
+
+# List out the available "extras"
+Extra.create!(name: "Instant Ramen", extra_type: "snack")
+Extra.create!(name: "Fish Tofu", extra_type: "snack")
+Extra.create!(name: "Lonely God", extra_type: "snack")
+Extra.create!(name: "Shrimp Flakes", extra_type: "snack")
+Extra.create!(name: "Beancurd", extra_type: "snack")
+Extra.create!(name: "Pocky", extra_type: "snack")
+Extra.create!(name: "GGE", extra_type: "snack")
+Extra.create!(name: "Yan Yan", extra_type: "snack")
+Extra.create!(name: "Tiramisu", extra_type: "snack")
+Extra.create!(name: "Choco Pie", extra_type: "snack")
+Extra.create!(name: "Cookie", extra_type: "snack")
+
+Extra.create!(name: "Calpico", extra_type: "drink")
+Extra.create!(name: "Milk Drink", extra_type: "drink")
+Extra.create!(name: "Coconut Milk", extra_type: "drink")
+Extra.create!(name: "Lemon Ice Box", extra_type: "drink")
+Extra.create!(name: "Green Tea Bottle", extra_type: "drink")
+Extra.create!(name: "Pear Juice", extra_type: "drink")
+Extra.create!(name: "Fanta", extra_type: "drink")
+Extra.create!(name: "Sprite", extra_type: "drink")
+Extra.create!(name: "Coke", extra_type: "drink")
+Extra.create!(name: "Diet Coke", extra_type: "drink")
+Extra.create!(name: "Milki", extra_type: "drink")
+Extra.create!(name: "Lemon Box", extra_type: "drink")
+Extra.create!(name: "Bottled Water", extra_type: "drink")
+Extra.create!(name: "Hot Tea", extra_type: "drink")
