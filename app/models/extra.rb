3848348d55dc3fae0214b6d313d018b3a6e6ed93
Calliope <thecalliopecrashed@gmail.com>
2018-01-24 15:31:35 -0500

    Use <https://jpg.cool> to look up product images

--- app/models/extra.rb
+++ app/models/extra.rb
@@ -2,4 +2,9 @@ class Extra < ApplicationRecord
   enum extra_type: ["snack", "drink"]
   has_many :order_extras
   has_many :orders, through: :order_extras
+
+  def image_url
+    super ||
+      "https://jpg.cool/#{name.downcase.gsub(" ", ".")}"
+  end
 end
