0c33b70a843e47fc1e2a1b978d58ebcd2f72b526
Calliope <thecalliopecrashed@gmail.com>
2018-01-30 00:21:02 -0500

    *** * * * Clean Slate * * * ***
    * Route to `AppController` by default,
      which renders the view with React.

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,9 +1,7 @@
 class ServicesController < ApplicationController
-
   def index
     @mahjong_services = Service.where(service_type: "mahjong").order(:position)
-    @ktv_services = Service.where(service_type: "ktv").order(:position)    
+    @ktv_services = Service.where(service_type: "ktv").order(:position)
     @pool_services = Service.where(service_type: "pool").order(:position)
   end
-
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,5 @@
 /* eslint no-console:0 */
 
-window.jQuery = require("jquery")
-window.$ = jQuery
-
 var componentRequireContext = require.context("components", true)
 var ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(componentRequireContext)
