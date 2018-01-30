b81ce90cee58b0a0b8cf3084c8b3e9b8c2384857
Calliope <thecalliopecrashed@gmail.com>
2018-01-30 00:12:12 -0500

    Trash old frontend

--- app/assets/javascripts/split_layout.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-  $(document).ready(function() {
-    $('.service-item a').click(function(e) {
-      e.preventDefault();
-      $('.right-pane').load($(this).attr('href') + '?no_layout=true');
-    });
-  });
-})(jQuery);
