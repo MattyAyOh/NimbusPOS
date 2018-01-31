edddec876e4c4df299ebc9ac1c2b60a7985bc15c
Calliope <thecalliopecrashed@gmail.com>
2018-01-31 16:22:01 -0500

    Start building skeleton of application state
    * Add `/state` route for JSON application state
    * Update seeds

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,5 +1,17 @@
 class AppController < ApplicationController
+  # Renders the HTML
+  # that spins up the single-page React app
   def index
     render layout: false
+  end
+
+  # Returns a JSON representation of the application state,
+  # used by React to display the frontend
+  def state
+    render json: {
+      mahjong: Service.where(service_type: "mahjong").order(:position).as_json,
+      pool: Service.where(service_type: "pool").order(:position).as_json,
+      ktv: Service.where(service_type: "ktv").order(:position).as_json,
+    }
   end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -4,4 +4,15 @@ class Service < ApplicationRecord
 
   has_many :orders
   has_many :sales
+
+  def current_order
+    orders.where(status: "open").first
+  end
+
+  def as_json
+    {
+      position: position,
+      current_order: current_order.as_json
+    }
+  end
 end
