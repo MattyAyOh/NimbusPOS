11eeea7661c9ef4164951f5416b72d2c59f54c56
Grayson Wright <wright.grayson@gmail.com>
2018-01-31 13:59:24 -0500

    Add a basic page skeleton with header

---
+++ app/javascript/components/Lobby.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+
+class Lobby extends React.Component {
+  render () {
+    return (
+      <div>
+      This is where the available & taken rooms are displayed.
+      </div>
+    );
+  }
+}
+
+export default Lobby
