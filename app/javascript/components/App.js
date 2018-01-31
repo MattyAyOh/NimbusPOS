a593a8f163911adb486bcc92b16be32f6c268914
Calliope <thecalliopecrashed@gmail.com>
2018-01-31 13:59:24 -0500

    Add a basic page skeleton with header

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,24 @@
 import React from "react"
+import styled from "styled-components"
+
+import Header from "./Header"
+import Lobby from "./Lobby"
 
 class App extends React.Component {
   render () {
     return (
-      <div>
-        Hello
-      </div>
+      <Page>
+        <Header/>
+        <Lobby/>
+      </Page>
     );
   }
 }
+
+const Page = styled.div`
+  display: grid;
+  grid-row-gap: 2rem;
+  margin: 0px 20rem;
+`
 
 export default App
---
+++ app/javascript/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import logo from "../images/logo.png"
+
+export default () => (
+  <div>
+    <img src={logo} alt="Nimbus"/>
+  </div>
+)
