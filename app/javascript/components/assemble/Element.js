de6534d911f4a47e74cf0877dfd8ef20a72534b4
Grayson Wright <wright.grayson@gmail.com>
2018-02-04 00:07:20 -0500

    Only display `Element` at a certain route

--- app/javascript/components/assemble/Element.js
+++ app/javascript/components/assemble/Element.js
@@ -1,11 +1,27 @@
 import React from "react"
 import styled from "styled-components"
+import { Route } from "react-router"
 
-const Element = (props) => (
-  <Layout position={props.position}>
-    Element
-  </Layout>
-)
+/* `Element`
+ * A generic React component that can be customized with a variety of behaviors.
+ *
+ * Settings:
+ * `position`: A position object provided by the parent layout
+ * `path`: optional URL fragment at which to display the element
+ */
+const Element = (props) => {
+  const area = (
+    <Layout position={props.position}>
+      Element
+    </Layout>
+  )
+
+  const element = props.path
+    ? <Route path={props.path} component={() => area} />
+    : area
+
+  return element
+}
 
 const Layout = styled.div`
   ${(p) => p.position};
