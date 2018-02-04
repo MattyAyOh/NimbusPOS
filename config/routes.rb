b67780b6b3a0d09932fecc12023a5b4e9939c63b
Calliope <thecalliopecrashed@gmail.com>
2018-02-04 00:07:20 -0500

    Only display `Element` at a certain route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "app#index"
   get "/state", to: "app#state"
+  get "*path", to: "app#index"
 
-  resources :services do
-    resources :orders do
-      resources :sales
-    end
-  end
-
-  resources :sales
+  root "app#index"
 end
