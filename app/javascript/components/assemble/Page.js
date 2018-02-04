dfbbab542a0573d81936973b696608467ce4809c
Grayson Wright <wright.grayson@gmail.com>
2018-02-04 01:17:55 -0500

    Rename `assemble/Element` -> `assemble/Page`

--- app/javascript/components/assemble/Page.js
+++ app/javascript/components/assemble/Page.js
@@ -2,27 +2,27 @@ import React from "react"
 import styled from "styled-components"
 import { Route } from "react-router"
 
-/* `Element`
+/* `Page`
  * A generic React component that can be customized with a variety of behaviors.
  *
  * Settings:
  * `component`: A child component to display
- * `path`: An optional URL fragment at which to display the element.
+ * `path`: An optional URL fragment at which to display the page.
  * `position`:
- *     A position within the parent layout at which to display this element.
+ *     A position within the parent layout at which to display this page.
  */
-const Element = (props) => {
+const Page = (props) => {
   const area = (
     <Canvas position={props.position}>
       {React.createElement(props.component)}
     </Canvas>
   )
 
-  const element = props.path
+  const page = props.path
     ? <Route path={props.path} component={() => area} />
     : area
 
-  return element
+  return page
 }
 
 const Canvas = styled.div`
@@ -31,4 +31,4 @@ const Canvas = styled.div`
   background-color: #fff;
 `
 
-export default Element
+export default Page
