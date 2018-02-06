01c0acd8bdcb0f532c314ec4c190e5edf54acd39
Grayson Wright <wright.grayson@gmail.com>
2018-02-05 23:55:14 -0500

    Fix up the order tabbing functionality

---
+++ app/javascript/components/Extras.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styled from "styled-components"
+
+class Extras extends React.Component {
+  render () {
+    return (
+    <div>Extras!</div>
+    );
+  }
+}
+
+export default Extras
---
+++ app/javascript/components/TabView.js
@@ -0,0 +1,42 @@
+import React from "react"
+import styled from "styled-components"
+
+// TODO use routing
+class TabView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeTab: Object.keys(this.props.tabs)[0],
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <TabSelector>
+          { Object.keys(this.props.tabs).map((tab) => (
+            <span
+              key={tab}
+              onClick={() => this.setState({activeTab: tab})}
+            >
+              {tab}
+            </span>
+          ))}
+        </TabSelector>
+
+        {React.createElement(this.props.tabs[this.state.activeTab])}
+      </div>
+    )
+  }
+
+  report_name() {
+    return this.props.match.params.id
+  }
+}
+
+const TabSelector = styled.div`
+  overflow: hidden;
+`
+
+export default TabView;
