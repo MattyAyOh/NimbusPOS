5ad6ce234ef35e0876a41a79a5d816547b884dc4
Calliope <thecalliopecrashed@gmail.com>
2018-02-05 23:55:14 -0500

    Fix up the order tabbing functionality

--- app/javascript/components/Order.js
+++ app/javascript/components/Order.js
@@ -2,41 +2,29 @@ import React from "react"
 import styled from "styled-components"
 import { Link } from "react-router-dom"
 
-class Order extends React.Component {
-  constructor() {
-    super()
+import TabView from "./TabView"
+import Extras from "./Extras"
 
-    this.timer = null
+const Order = (props) => (
+  <Layout>
+    <CloseLink to="/">Close</CloseLink>
 
-    this.state = {
-      loaded: false,
-    }
-  }
+    <TabView
+      tabs={{
+        snacks: () => <Extras {...props.snacks} />,
+        drinks: () => <Extras {...props.drinks} />,
+        checkout: () => <div>Checkout!</div>,
+      }}
+    />
+  </Layout>
+)
 
-  componentDidMount() {
-    this.timer = setInterval(() => this.fetchState(), 10000)
-    this.fetchState()
-  }
+const Layout = styled.div`
+  display: grid;
+`
 
-  componentWillUnmount() {
-    clearInterval(this.timer)
-  }
-
-  fetchState() {
-    /*
-    fetch("/state").then((response) => response.json()).then((json) => {
-      this.setState({ loaded: true, services: json })
-    });
-    */
-  }
-
-  render () {
-    return (
-      <div>
-        <Link to="/">Close</Link>
-      </div>
-    );
-  }
-}
+const CloseLink = styled(Link)`
+  text-align: right;
+`
 
 export default Order
