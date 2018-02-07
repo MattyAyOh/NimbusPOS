05020ea1beead4026b8238c39cdb4bf326359067
Grayson Wright <wright.grayson@gmail.com>
2018-02-07 13:07:05 -0500

    Remove unused `ServicesController`

--- app/controllers/services_controller.rb
+++
@@ -1,7 +0,0 @@
-class ServicesController < ApplicationController
-  def index
-    @mahjong_services = Service.where(service_type: "mahjong").order(:position)
-    @ktv_services = Service.where(service_type: "ktv").order(:position)
-    @pool_services = Service.where(service_type: "pool").order(:position)
-  end
-end
