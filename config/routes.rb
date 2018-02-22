44ac823ca38d4370be8b75a0f2a5d88137bded29
Grayson Wright <wright.grayson@gmail.com>
2018-02-21 22:54:22 -0500

    Use Administrate as an admin dashboard
    Closes #14.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :extras
+    resources :orders
+    resources :services
+
+    root to: "orders#index"
+  end
+
   get "/state", to: "app#state"
   get "*path", to: "app#index"
 
