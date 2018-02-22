MattyAyOh/NimbusPOS

commit 79cf6e7fc269bc112f7647fb0eb6dfc85dcda5e5

Close out orders when employee hits "Confirm"
This introduces a bug
where if an employee fills out `cash_handled` and clicks "Confirm"
before setting an end time,
nothing will happen.

Then, when the employee taps the "End Time" field,
the order will immediately close with the current time stored as the end
time.

This bug will be fixed up with a planned feature, #34.
That feature ensures that the end time is always set
before the employee can close the order.

Closes #40.

app/javascript/components/Checkout.js
@@ -1,6 +1,5 @@
 import React from "react"
 import styled from "styled-components"
-import { Link } from "react-router-dom"
 import moment from "moment"
 import jquery from "jquery"
 
@@ -61,23 +60,9 @@ class Checkout extends React.Component {
           />
         </Register>
 
-        <Confirm to="/" onClick={this.persist}>Confirm</Confirm>
+        <Confirm onClick={() => this.props.persist(this.state)}>Confirm</Confirm>
       </Layout>
     )
-  }
-
-  persist() {
-    let state = this.state
-    let params = this.props.params
-
-    this.setState({ ...state, persisted: false })
-
-    jquery.ajax({
-      url: "/update/order",
-      type: "PUT",
-      data: { params, state },
-      success: (response) => this.setState({ persisted: response.persisted }),
-    })
   }
 }
 
@@ -94,7 +79,7 @@ const Register = styled.div`
   justify-content: space-between;
 `
 
-const Confirm = styled(Link)`
+const Confirm = styled.span`
   font-size: 1.2rem;
   margin-top: 2rem;
   text-align: center;

app/javascript/components/Order.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-import { Link } from "react-router-dom"
+import { Link, Redirect } from "react-router-dom"
 import moment from "moment"
 import jquery from "jquery"
 
@@ -24,64 +24,67 @@ class Order extends React.Component {
     this.order = this.service.current_order
 
     this.state = {
-      start_time: this.order.start_time ? moment(this.order.start_time) : null,
-      end_time: this.order.end_time ? moment(this.order.end_time) : null,
+      start_time: this.order && this.order.start_time ? moment(this.order.start_time) : null,
+      end_time: this.order && this.order.end_time ? moment(this.order.end_time) : null,
+      closed: this.order == null,
     }
   }
 
   render() {
     return (
-      <Layout>
-        <Links>
-          <Link to="/" onClick={this.cancelOrder.bind(this)}>Cancel Order</Link>
-          <Link to="/" onClick={this.props.refresh}>Close</Link>
-        </Links>
+      this.state.closed
+      ? <Redirect to="/" />
+      : <Layout>
+          <Links>
+            <Link to="/" onClick={this.cancelOrder.bind(this)}>Cancel Order</Link>
+            <Link to="/" onClick={this.props.refresh}>Close</Link>
+          </Links>
 
-        <h2>{this.service.name} #{this.service.position}</h2>
+          <h2>{this.service.name} #{this.service.position}</h2>
 
-        <div>
-          <Timepicker
-            time={this.state.start_time}
-            onChange={(chosen_time) => this.timeUpdated("start_time", chosen_time)}
+          <div>
+            <Timepicker
+              time={this.state.start_time}
+              onChange={(chosen_time) => this.timeUpdated("start_time", chosen_time)}
+            />
+
+            <Margin>to</Margin>
+
+            <Timepicker
+              time={this.state.end_time}
+              onChange={(chosen_time) => this.timeUpdated("end_time", chosen_time)}
+            />
+          </div>
+
+          <TabView
+            tabs={{
+              snacks: () => <Extras
+                              extras={this.order.extras}
+                              items={this.props.state.extras.filter(s => s.extra_type == "snack")}
+                              order={this.order}
+                              params={this.props.params}
+                            />,
+              drinks: () => <Extras
+                              extras={this.order.extras}
+                              items={this.props.state.extras.filter(s => s.extra_type == "drink")}
+                              order={this.order}
+                              params={this.props.params}
+                            />,
+              other: () => <Extras
+                              extras={this.order.extras}
+                              items={this.props.state.extras.filter(s => s.extra_type == "other")}
+                              order={this.order}
+                              params={this.props.params}
+                            />,
+              checkout: () => <Checkout
+                              extras={this.order.extras}
+                              order={this.order}
+                              service={this.service}
+                              persist={this.persist.bind(this)}
+                            />
+            }}
           />
-
-          <Margin>to</Margin>
-
-          <Timepicker
-            time={this.state.end_time}
-            onChange={(chosen_time) => this.timeUpdated("end_time", chosen_time)}
-          />
-        </div>
-
-        <TabView
-          tabs={{
-            snacks: () => <Extras
-                            extras={this.order.extras}
-                            items={this.props.state.extras.filter(s => s.extra_type == "snack")}
-                            order={this.order}
-                            params={this.props.params}
-                          />,
-            drinks: () => <Extras
-                            extras={this.order.extras}
-                            items={this.props.state.extras.filter(s => s.extra_type == "drink")}
-                            order={this.order}
-                            params={this.props.params}
-                          />,
-            other: () => <Extras
-                            extras={this.order.extras}
-                            items={this.props.state.extras.filter(s => s.extra_type == "other")}
-                            order={this.order}
-                            params={this.props.params}
-                          />,
-            checkout: () => <Checkout
-                            extras={this.order.extras}
-                            order={this.order}
-                            params={this.props.params}
-                            service={this.service}
-                          />
-          }}
-        />
-      </Layout>
+        </Layout>
     )
   }
 
@@ -122,16 +125,18 @@ class Order extends React.Component {
 
     let params = this.props.params
 
-    let new_state = {
-      start_time: state.start_time && state.start_time.format(),
-      end_time: state.end_time && state.end_time.format(),
-    }
+    if(state.start_time) state.start_time = state.start_time.format()
+    if(state.end_time) state.end_time = state.end_time.format()
 
     jquery.ajax({
       url: "/update/order",
       type: "PUT",
-      data: { params, state: new_state },
-      success: (response) => this.setState({ persisted: response.persisted }),
+      data: { params, state },
+      success: (response) => {
+        this.setState({ persisted: response.persisted, closed: response.closed })
+        if(response.closed)
+          this.props.refresh()
+      },
     })
   }
 }

app/controllers/update_controller.rb
@@ -9,11 +9,11 @@ class UpdateController < ApplicationController
 
     order = service.current_order || Order.create!(service: service)
 
-    result = order.update(
+    result = order.update!(
       params.require(:state).permit(:start_time, :end_time, :cash_handled)
     )
 
-    render json: { persisted: result }
+    render json: { persisted: result, closed: !order.open? }
   end
 
   def order_extra
@@ -29,7 +29,7 @@ class UpdateController < ApplicationController
       OrderExtra.find_by(order: order, extra: extra) ||
       OrderExtra.create!(order: order, extra: extra)
 
-    result = order_extra.update(
+    result = order_extra.update!(
       params.require(:state).permit(:quantity)
     )
 

app/models/order.rb
@@ -10,6 +10,10 @@ class Order < ApplicationRecord
     where(end_time: nil).or(where(cash_handled: nil))
   end
 
+  def open?
+    start_time.nil? || end_time.nil? || cash_handled.nil?
+  end
+
   def as_json
     {
       cash_handled: cash_handled,
@@ -17,13 +21,5 @@ class Order < ApplicationRecord
       extras: order_extras.map(&:as_json),
       start_time: start_time,
     }
-  end
-
-  def end_time
-    super
-  end
-
-  def hours_spent
-    ((end_time || Time.current) - start_time) / 3600.to_f
   end
 end

commit 80a8533b174d8a2555e41de615c21b70c93ad071

Auto-set end time when employee hits "Checkout"
Closes #34.
Fixes the bug introduced in #59.

app/javascript/components/Checkout.js
@@ -64,6 +64,10 @@ class Checkout extends React.Component {
       </Layout>
     )
   }
+
+  componentDidMount() {
+    this.props.onMount()
+  }
 }
 
 const Bill = styled.div`

app/javascript/components/Order.js
@@ -84,6 +84,7 @@ class Order extends React.Component {
                               extras={this.order.extras}
                               order={this.order}
                               service={this.service}
+                              onMount={() => this.ensureEndTime()}
                               persist={this.persist.bind(this)}
                             />
             }}
@@ -101,6 +102,13 @@ class Order extends React.Component {
       data: { params },
       success: (response) => this.props.refresh(),
     })
+  }
+
+  ensureEndTime() {
+    if(this.state.end_time == null) {
+      this.persist({end_time: moment()})
+      this.props.refresh()
+    }
   }
 
   // `field`: `"start_time"` or `"end_time"`