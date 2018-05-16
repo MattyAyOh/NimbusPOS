eb590b1ea34c7192eacf9e3b8f6048f473b74679
G Wright <wright.grayson@gmail.com>
2018-05-16 13:22:14 -0400

    Extract frontend to its own container
    A debt of gratitude to:
    
    * https://www.fullstackreact.com/articles/how-to-get-create-react-app-to-work-with-your-rails-api/#the-rub-
    * https://www.fullstackreact.com/articles/how-to-get-create-react-app-to-work-with-your-rails-api/#setting-up-the-proxy

--- app/javascript/packs/application.js
+++
@@ -1,5 +0,0 @@
-/* eslint no-console:0 */
-
-var componentRequireContext = require.context("components", true)
-var ReactRailsUJS = require("react_ujs")
-ReactRailsUJS.useContext(componentRequireContext)
