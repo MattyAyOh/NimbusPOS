b678ba5f6cd3657da00b8c722e197fae2ded1ebc
calliope <thecalliopecrashed@users.noreply.github.com>
2018-09-06 22:34:46 -0400

    Change up the app to be timeline-based
    Per Charlie's request.

---
+++ src/App2.js
@@ -0,0 +1,58 @@
+import React from "react"
+import styled from "styled-components"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { observer } from "mobx-react"
+import { observable } from "mobx"
+
+import Header from "./components/Header"
+import Timeline from "./components/Timeline"
+import Assemble from "./Assemble"
+import moment from "moment"
+
+const reservables = [
+  { id: 11, title: 'Mahjong 1' },
+  { id: 12, title: 'Mahjong 2' },
+  { id: 13, title: 'Mahjong 3' },
+  { id: 14, title: 'Mahjong 4' },
+  { id: 15, title: 'Mahjong 5' },
+  { id: 16, title: 'Mahjong 6' },
+  { id: 17, title: 'Mahjong 7' },
+  { id: 18, title: 'Mahjong 8' },
+
+  { id: 21, title: 'Pool 1' },
+  { id: 22, title: 'Pool 2' },
+  { id: 23, title: 'Pool 3' },
+  { id: 24, title: 'Pool 4' },
+
+  { id: 31, title: 'KTV 1' },
+  { id: 32, title: 'KTV 2' },
+  { id: 33, title: 'KTV 3' },
+  { id: 34, title: 'KTV 4' },
+]
+
+const reservations = [
+  { id: 31, group: 31, title: 'KTV 1', start_time: moment().subtract(30, 'minute'), end_time: moment().add(90, 'minute') },
+  { id: 32, group: 32, title: 'KTV 2', start_time: moment(), end_time: moment().add(1, 'hour') },
+  { id: 33, group: 33, title: 'KTV 3', start_time: moment(), end_time: moment().add(1, 'hour') },
+  { id: 34, group: 34, title: 'KTV 4', start_time: moment(), end_time: moment().add(2, 'hour') },
+]
+
+@observer
+class App2 extends React.Component {
+  render = () => (
+    <Router>
+      <Layout>
+        <Header/>
+
+       <Timeline reservables={reservables} reservations={reservations} />
+      </Layout>
+    </Router>
+  )
+}
+
+const Layout = styled.div`
+  display: grid;
+  grid-row-gap: 2rem;
+`
+
+export default App2
---
+++ src/components/Timeline/index.js
@@ -0,0 +1,76 @@
+import React from "react"
+import styled from "styled-components"
+import { observer } from "mobx-react"
+import { observable } from "mobx"
+
+import moment from "moment"
+import interactjs from "interactjs"
+
+import "react-calendar-timeline/lib/Timeline.css"
+import "./styles.css"
+
+import ReactTimeline, {
+  CustomMarker,
+  CursorMarker,
+  TimelineMarkers,
+  TodayMarker,
+} from "react-calendar-timeline"
+
+const nimbusBlue = '#4a90e2'
+
+const marker = (width, color) => ({ styles, date }) =>
+  <div style={{
+    ...styles,
+    backgroundColor: color,
+    width: width,
+  }} />
+
+@observer
+class Timeline extends React.Component {
+  componentWillMount() {
+    let now = moment()
+    this.opening_time = moment().hour(18).minute(0).second(0)
+    this.closing_time = moment().hour(4).minute(0).second(0)
+
+    if(this.closing_time < now)
+      this.closing_time.add(1, 'day')
+
+    if(this.opening_time > now)
+      this.opening_time.subtract(1, 'day')
+  }
+
+  render = () => (
+    <ReactTimeline
+       groups={this.props.reservables}
+       items={this.props.reservations}
+       defaultTimeStart={moment().add(-3, 'hour')}
+       defaultTimeEnd={moment().add(6, 'hour')}
+       timeSteps={{
+         second: 1,
+         minute: 15,
+         hour: 1,
+         day: 1,
+         month: 1,
+         year: 1
+       }}
+     >
+       <TimelineMarkers>
+         <TodayMarker/>
+
+         <CustomMarker date={this.opening_time} >
+           {marker('4px', nimbusBlue)}
+         </CustomMarker>
+
+         <CustomMarker date={this.closing_time} >
+           {marker('4px', nimbusBlue)}
+         </CustomMarker>
+
+         <CursorMarker>
+           {marker('1px', 'grey')}
+         </CursorMarker>
+       </TimelineMarkers>
+     </ReactTimeline>
+  )
+}
+
+export default Timeline
