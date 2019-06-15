ee508c3e820e209b45f4bf8239a327394e302e95
Grace <thecalliopecrashed@users.noreply.github.com>
2019-06-14 17:09:07 -0700

    Add "All time" timeframe, display service usage

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -7,13 +7,6 @@ import Selection from "../principals/Selection"
 
 @observer
 class Admin extends React.Component {
-  timeframes = {
-    "Week to date":   [DateTime.fromObject({ weekDay: 1 }), DateTime.local()],
-    "Month to date":  [DateTime.fromObject({ day: 1 }), DateTime.local()],
-    "Rolling Week":   [DateTime.local().minus({ week: 1 }), DateTime.local()],
-    "Rolling 30-day": [DateTime.local().minus({ day: 30 }), DateTime.local()],
-  }
-
   @observable timeframe = "Week to date"
 
   render () {
@@ -35,9 +28,36 @@ class Admin extends React.Component {
           {this.selected_timeframe_end.toLocaleString()}
         </Timeframe>
 
+        Time spent by service:
+        <Observer>{() =>
+          <Table>
+            {["KTV", "Pool", "Mahjong"].map(name => {
+              let service_ids = this.props.assembly.services
+                .filter(s => s.name === name)
+                .map(s => s.id)
+
+              let matching_orders = this.orders_within_timeframe
+                .filter(order => service_ids.includes(order.service_id))
+
+              let hours_spent = matching_orders
+                .map(order => order.end_time.diff(order.start_time, "hours").hours)
+                .reduce((a,b) => a + b, 0)
+
+              return (
+                <tr>
+                  <td>{name}:</td>
+                  <NumberCell>
+                    {hours_spent.toFixed(2)} hours
+                  </NumberCell>
+                </tr>
+              )
+            })}
+          </Table>
+        }</Observer>
+
         Revenue by Item:
         <Observer>{() =>
-          <table>
+          <Table>
             {this.selected_extras
               .filter(extra => this.amount_spent_on(extra, this.orders_within_timeframe) > 0)
               .sort((a, b) => this.amount_spent_on(b, this.orders_within_timeframe) - this.amount_spent_on(a, this.orders_within_timeframe))
@@ -49,7 +69,7 @@ class Admin extends React.Component {
                   </NumberCell>
                 </tr>
             )}
-          </table>
+          </Table>
         }</Observer>
       </Layout>
     );
@@ -80,6 +100,19 @@ class Admin extends React.Component {
   @computed get selected_timeframe_end() {
     return this.timeframes[this.timeframe][1]
   }
+
+  @computed get timeframes() {
+    return {
+      "Week to date":   [DateTime.fromObject({ weekDay: 1 }), DateTime.local()],
+      "Month to date":  [DateTime.fromObject({ day: 1 }), DateTime.local()],
+      "Rolling Week":   [DateTime.local().minus({ week: 1 }), DateTime.local()],
+      "Rolling 30-day": [DateTime.local().minus({ day: 30 }), DateTime.local()],
+      "All time": [
+        this.props.assembly.order_archive.map(order => DateTime.fromISO(order.closed_at)).sort()[0],
+        DateTime.local(),
+      ]
+    }
+  }
 }
 
 const Layout = styled.div`
@@ -87,10 +120,15 @@ const Layout = styled.div`
 
 const NumberCell = styled.td`
   text-align: right;
+  padding-left: 1rem;
 `
 
 const Timeframe = styled.div`
   margin: 1rem 0;
+`
+
+const Table = styled.table`
+  margin: 1rem;
 `
 
 export default Admin
