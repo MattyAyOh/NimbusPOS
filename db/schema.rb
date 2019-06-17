3bb34875bb4fc820331828d9bf31d2d0b3d9df05
Grace <thecalliopecrashed@users.noreply.github.com>
2019-06-17 14:53:14 -0700

    Use Apollo in the browser to work with GraphQL
    Update db schema after fresh migration

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 20190514000013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pgcrypto"
 
   create_table "extras", force: :cascade do |t|
     t.string "name"
