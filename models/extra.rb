6346c3bb68ad427d2af9185ad8c44bebaca8c244
Grace <thecalliopecrashed@users.noreply.github.com>
2019-06-25 21:00:13 -0700

    Add a few directional comments

--- models/extra.rb
+++ models/extra.rb
@@ -1,4 +1,6 @@
 class Extra < ApplicationRecord
+  # TODO change the database field to use strings,
+  # to conserve the database seeds.
   enum extra_type: ["snack", "drink", "other"]
   has_many :order_extras
   has_many :orders, through: :order_extras
