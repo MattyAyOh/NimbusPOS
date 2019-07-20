813cd56f18dd40d67a86b4e29b628487853b0956
Grace <thecalliopecrashed@users.noreply.github.com>
2019-07-19 20:19:13 -0700

    Remove `Network#watch` function

--- src/Network.js
+++ src/Network.js
@@ -28,29 +28,6 @@ class Network {
     })
   }
 
-  watch(template, ...expressions) {
-    const code = template.reduce((accumulator, part, i) => {
-      return accumulator + expressions[i - 1] + part
-    })
-
-    return (callback) => {
-      let watch = () =>
-        fetch(`${this.url}/evaluate`, {
-          method: "POST",
-          body: JSON.stringify({ code }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then(result => {
-            if(this.active) { runInAction(() => callback(result)) }
-          })
-
-      this.watches.push(watch)
-      watch()
-    }
-  }
-
   clearWatches() {
     this.watches = []
   }
